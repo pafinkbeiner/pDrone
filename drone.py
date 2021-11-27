0372003aec35653f2aceef6b804057acd8b4c124
Paul Finkbeiner <paul-d-f@web.de>
2021-11-27 08:31:14 +0100

    projekt arch. update

---
+++ drone.py
@@ -0,0 +1,238 @@
+from flask import Flask
+from flask import request
+import time
+import json
+import logging
+from PyAccessPoint import pyaccesspoint
+from threading import Thread
+import sys
+from dotenv import load_dotenv
+import os
+load_dotenv()
+if os.environ.get("env") == "production":
+    import control as control
+    import gyro as gyro
+else:
+    import control_test as control
+    import gyro_test as gyro
+
+###################### init ####################
+app = Flask(__name__)
+access_point = pyaccesspoint.AccessPoint(
+    wlan="wlan0", ssid="drone", password="12345678", netmask="255.255.255.252", ip="10.0.0.1")
+if os.environ.get("env") == "production":
+    access_point.start()
+logging.basicConfig(level=logging.DEBUG,
+                    format='[%(asctime)s]: {} %(levelname)s %(message)s'.format(
+                        os.getpid()),
+                    datefmt='%Y-%m-%d %H:%M:%S',
+                    handlers=[logging.StreamHandler()])
+logger = logging.getLogger()
+port = os.environ.get("port")
+application = {
+    'onFlight': False,
+    'stabilisationRate': 2
+}
+command = {
+    'vl': 0,
+    'vr': 0,
+    'hl': 0,
+    'hr': 0
+}
+motor = {
+    'vl': 1510,
+    'vr': 1510,
+    'hl': 1510,
+    'hr': 1510
+}
+gyroBase = {
+    "x": 0,
+    "y": 0,
+    "z": 0
+}
+
+###################### main ####################
+
+
+def setMotorState(newState):
+    motor.update(newState)
+    print("Updated motor values: ", motor)
+
+
+def initialize():
+    logger.log(1, "Initialize Startup Sequence")
+    check0 = True
+    if os.environ.get("env") == "production":
+        check0 = access_point.is_running()
+
+    logger.log(1, "Initialize gyro...")
+    # check correctness of gyro sensor
+    check1 = type(gyro.get_scaled_x_out()) == int or float
+    check2 = type(gyro.get_scaled_y_out()) == int or float
+    check3 = type(gyro.get_scaled_z_out()) == int or float
+    check4 = type(gyro.get_scaled_acc_x_out()) == int or float
+    check5 = type(gyro.get_scaled_acc_y_out()) == int or float
+    check6 = type(gyro.get_scaled_acc_z_out()) == int or float
+
+    # set current gyro position as default
+    gyroBase.update({
+        "x": gyro.get_scaled_x_out(),
+        "y": gyro.get_scaled_y_out(),
+        "z": gyro.get_scaled_z_out(),
+        "x_s": gyro.get_scaled_acc_x_out(),
+        "y_s": gyro.get_scaled_acc_y_out(),
+        "z_s": gyro.get_scaled_acc_z_out()
+    })
+
+    if check0 and check1 and check2 and check3 and check4 and check5 and check6:
+        logger.log(1, "Startup performed successfully")
+        return True
+    else:
+        logger.log(1, "Error occured while checking system")
+        return False
+
+
+def calibrate():
+    logger.log(1, "Initialize Calibrate")
+    check7 = control.calibrate()
+    return check7
+
+
+def arm():
+    logger.log(1, "Initialize arm")
+    check8 = control.arm()
+    return check8
+
+
+def stabilisation(x, y, z, prev=motor):
+    print("Stabilisation..")
+    # use access to gyroBase
+
+    return {'vl': prev['vl'], 'vr': prev['vr'], 'hl': prev['hl'], 'hr': prev['hr']}
+
+
+def flight():
+
+    while True:
+
+        # stops current thread
+        if application['onFlight'] == True:
+
+            last_time = round(time.time() * 1000)
+
+            # get gyro base information -> data & acc
+            gyrod = gyro.get_scaled_x_y_z_out()
+            gyroacc = gyro.get_scaled_acc_x_y_z_out()
+
+            # get gyro rotation information
+            rotx = gyro.get_x_rotation(
+                gyroacc['x'], gyroacc['y'], gyroacc['z'])
+            roty = gyro.get_y_rotation(
+                gyroacc['x'], gyroacc['y'], gyroacc['z'])
+
+            # get new stabilasation values
+            stabRes = stabilisation(
+                gyrod['x'], gyrod['y'], gyrod['z'], prev=motor)
+
+            # refresh motor state with control values and stabilist values
+            setMotorState({
+                'vl': stabRes['vl'] + command['vl'],
+                'vr': stabRes['vr'] + command['vr'],
+                'hl': stabRes['hl'] + command['hl'],
+                'hr': stabRes['hr'] + command['hr']
+            })
+
+            # set back control state after changing
+            command.update({
+                'vl': 0,
+                'vr': 0,
+                'hl': 0,
+                'hr': 0
+            })
+
+            print("Took: "+str(round(time.time() * 1000) - last_time)+"ms")
+
+        time.sleep(application["stabilisationRate"])
+
+
+def start_app():
+    app.run(host='0.0.0.0', port=port, debug=False)
+
+
+#################### threads ###################
+t1 = Thread(target=flight, name="Flight")
+t2 = Thread(target=start_app, name="Flask")
+
+##################### routes ###################
+
+
+@app.route("/")
+def home_route():
+    return json.dumps({
+        "routes": ["/init", "/command", "/command/<key>/<value>", "flight/<b>"]
+    })
+
+
+@app.route("/init")
+def init_route():
+    res = initialize()
+    return json.dumps(res)
+
+
+@app.route("/command", methods=['POST'])
+def command_route():
+    parsedData = request.data
+    print(parsedData)
+    # update motor
+    # command.update({
+    #     'vl': parsedData['vl'],
+    #     'vr': parsedData['vr'],
+    #     'hl': parsedData['hl'],
+    #     'hr': parsedData['hr']
+    # })
+    return json.dumps("Received")
+
+
+@app.route("/command/<key>/<value>")
+def command_alt_route(key, value):
+    print(f'Received {key} and {value}')
+    if key == "vl" or key == "vr" or key == "hl" or key == "hr":
+        command.update({
+            key: int(value)
+        })
+        return json.dumps("Received")
+    else:
+        return json.dumps("Wrong key")
+
+
+# /flight/1 od. /flight/0
+@app.route("/flight/<b>")
+def flight_route(b):
+    data = b
+    if data == "1":
+        # set motor to default before starting
+        motor.update({
+            'vl': 1510,
+            'vr': 1510,
+            'hl': 1510,
+            'hr': 1510
+        })
+        # start thread - if not already
+        if t1.is_alive() == False:
+            application['onFlight'] = True
+            t1.start()
+            return json.dumps("Started")
+        else:
+            application['onFlight'] = True
+            return json.dumps("Started")
+    else:
+        # stop flight thread
+        application['onFlight'] = False
+        return json.dumps("Stopped")
+
+@app.route("/kill")
+def kill_route():
+    sys.exit()
+
+###################### prod #####################
+t2.start()
---
+++ old/gyro.py
@@ -0,0 +1,136 @@
+#!/usr/bin/python
+
+# Activate I2C Bus
+# sudo raspi-config > ...
+# sudo apt-get install i2c-tools python-smbus
+# sudo i2cdetect -y 1
+
+# Pinbelegung (BCM)
+# Pin 1 (3.3V)	VCC
+# Pin 3 (SDA)	SDA
+# Pin 5 (SCL)	SCL
+# Pin 6 (GND)	GND
+
+import smbus
+import math
+import time
+ 
+# Register
+power_mgmt_1 = 0x6b
+power_mgmt_2 = 0x6c
+ 
+def read_byte(reg):
+    return bus.read_byte_data(address, reg)
+ 
+def read_word(reg):
+    h = bus.read_byte_data(address, reg)
+    l = bus.read_byte_data(address, reg+1)
+    value = (h << 8) + l
+    return value
+ 
+def read_word_2c(reg):
+    val = read_word(reg)
+    if (val >= 0x8000):
+        return -((65535 - val) + 1)
+    else:
+        return val
+ 
+def dist(a,b):
+    return math.sqrt((a*a)+(b*b))
+ 
+def get_y_rotation(x,y,z):
+    radians = math.atan2(x, dist(y,z))
+    return -math.degrees(radians)
+ 
+def get_x_rotation(x,y,z):
+    radians = math.atan2(y, dist(x,z))
+    return math.degrees(radians)
+ 
+#### unscaled ####
+def get_x_out():
+    return read_word_2c(0x43)
+
+def get_y_out():
+    return read_word_2c(0x45)
+
+def get_z_out():
+    return read_word_2c(0x47)
+
+def get_acc_x_out():
+    return read_word_2c(0x3b)
+
+def get_acc_y_out():
+    return read_word_2c(0x3d)
+
+def get_acc_z_out():
+    return read_word_2c(0x3f)
+
+#### scaled ####
+def get_scaled_x_y_z_out():
+    return { 'x': get_scaled_x_out(), 'y': get_scaled_y_out(), 'z': get_scaled_z_out()}
+
+def get_scaled_x_out():
+    return (read_word_2c(0x43) / 131)
+
+def get_scaled_y_out():
+    return (read_word_2c(0x45) / 131)
+
+def get_scaled_z_out():
+    return (read_word_2c(0x47) / 131)
+
+def get_scaled_acc_x_y_z_out():
+    return { 'x': get_scaled_acc_x_out(), 'y': get_scaled_acc_y_out(), 'z': get_scaled_acc_z_out()}
+
+def get_scaled_acc_x_out():
+    return (read_word_2c(0x3b) / 16384.0)
+
+def get_scaled_acc_y_out():
+    return (read_word_2c(0x3d) / 16384.0)
+
+def get_scaled_acc_z_out():
+    return (read_word_2c(0x3f) / 16384.0)
+
+bus = smbus.SMBus(1) # bus = smbus.SMBus(0) fuer Revision 1
+address = 0x68       # via i2cdetect
+ 
+# Aktivieren, um das Modul ansprechen zu koennen
+bus.write_byte_data(address, power_mgmt_1, 0)
+ 
+# print( "Gyroskop")
+# print( "--------")
+ 
+# gyroskop_xout = read_word_2c(0x43)
+# gyroskop_yout = read_word_2c(0x45)
+# gyroskop_zout = read_word_2c(0x47)
+ 
+# print( "gyroskop_xout: ", ("%5d" % gyroskop_xout), " skaliert: ", (gyroskop_xout / 131))
+# print( "gyroskop_yout: ", ("%5d" % gyroskop_yout), " skaliert: ", (gyroskop_yout / 131))
+# print( "gyroskop_zout: ", ("%5d" % gyroskop_zout), " skaliert: ", (gyroskop_zout / 131))
+ 
+# print
+# print ("Beschleunigungssensor")
+# print ("---------------------")
+ 
+# beschleunigung_xout = read_word_2c(0x3b)
+# beschleunigung_yout = read_word_2c(0x3d)
+# beschleunigung_zout = read_word_2c(0x3f)
+ 
+# beschleunigung_xout_skaliert = beschleunigung_xout / 16384.0
+# beschleunigung_yout_skaliert = beschleunigung_yout / 16384.0
+# beschleunigung_zout_skaliert = beschleunigung_zout / 16384.0
+ 
+# print ("beschleunigung_xout: ", ("%6d" % beschleunigung_xout), " skaliert: ", beschleunigung_xout_skaliert)
+# print ("beschleunigung_yout: ", ("%6d" % beschleunigung_yout), " skaliert: ", beschleunigung_yout_skaliert)
+# print ("beschleunigung_zout: ", ("%6d" % beschleunigung_zout), " skaliert: ", beschleunigung_zout_skaliert)
+ 
+# print ("X Rotation: " , get_x_rotation(beschleunigung_xout_skaliert, beschleunigung_yout_skaliert, beschleunigung_zout_skaliert))
+# print ("Y Rotation: " , get_y_rotation(beschleunigung_xout_skaliert, beschleunigung_yout_skaliert, beschleunigung_zout_skaliert))
+
+# while True:
+#     print("Sensor X: "+str(read_word_2c(0x43)))
+#     print("Sensor X: "+str(read_word_2c(0x45)))
+#     print("Sensor X: "+str(read_word_2c(0x47)))
+#     print ("X Rotation: " , get_x_rotation(beschleunigung_xout_skaliert, beschleunigung_yout_skaliert, beschleunigung_zout_skaliert))
+#     print ("Y Rotation: " , get_y_rotation(beschleunigung_xout_skaliert, beschleunigung_yout_skaliert, beschleunigung_zout_skaliert))
+#     time.sleep(1)
+#     print("---------------------------------------")
