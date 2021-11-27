4934093bd6af7bebf4e31968fe2cf7d6f44cfec4
Paul Finkbeiner <paul-d-f@web.de>
2021-11-27 13:46:52 +0100

    sc

--- drone.py
+++ drone.py
@@ -183,12 +183,12 @@ def init_route():
     return json.dumps(res)
 
 @app.route("/calibrate")
-def init_route():
+def icalibrate_route():
     res = control.calibrate()
     return json.dumps("true")
 
 @app.route("/arm")
-def init_route():
+def arm_route():
     res = control.arm()
     return json.dumps("true")
 
