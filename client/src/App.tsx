1bbaa767c432efde2bd29dd13d61a327169c4217
pafinkbeiner <paul-d-f@web.de>
2021-11-27 07:45:52 +0100

    sc

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,18 +1,9 @@
 import React from 'react';
 import logo from './logo.svg';
-import { io } from "socket.io-client";
+import axios from 'axios';
 import './App.css';
 
-// const socket = io("http://localhost:8080", {  transports: ["websocket"] });
-export const socket = io("http://localhost:8080");
-
-socket.on("connect", () => {
-  console.log("Connected to the server");
-});
-
-socket.on("disconnect", () => {
-  console.log("Disconected from the server");
-});
+const URL = "http://localhost:8080"
 
 function App() {
   return (
@@ -21,26 +12,26 @@ function App() {
 
       <div style={{backgroundColor: "red", borderRadius: "6px", padding: "1rem"}} className="vl">
         <h2>VL</h2>
-        <button onClick={() => socket.emit("command", JSON.stringify({ 'vl': 5, 'vr': 0, 'hl': 0, 'hr': 0 }))} style={{height: "3rem", width: "6rem"}}>+</button>
-        <button onClick={() => socket.emit("command", JSON.stringify({ 'vl': -5, 'vr': 0, 'hl': 0, 'hr': 0 }))} style={{height: "3rem", width: "6rem"}}>-</button>
+        <button onClick={() => {axios.get(`${URL}/command/vl/5`)}} style={{height: "3rem", width: "6rem"}}>+</button>
+        <button onClick={() => {axios.get(`${URL}/command/vl/-5`)}} style={{height: "3rem", width: "6rem"}}>-</button>
       </div>
       <br/>
       <div style={{backgroundColor: "blue", borderRadius: "6px", padding: "1rem"}} className="hl">
         <h2>HL</h2>
-        <button onClick={() => socket.emit("command", JSON.stringify({ 'vl': 0, 'vr': 0, 'hl': 5, 'hr': 0 }))} style={{height: "3rem", width: "6rem"}}>+</button>
-        <button onClick={() => socket.emit("command", JSON.stringify({ 'vl': 0, 'vr': 0, 'hl': -5, 'hr': 0 }))} style={{height: "3rem", width: "6rem"}}>-</button>
+        <button onClick={() => {axios.get(`${URL}/command/hl/5`)}} style={{height: "3rem", width: "6rem"}}>+</button>
+        <button onClick={() => {axios.get(`${URL}/command/hl/-5`)}} style={{height: "3rem", width: "6rem"}}>-</button>
       </div>
       <br/>
       <div style={{backgroundColor: "green", borderRadius: "6px", padding: "1rem"}} className="vr">
         <h2>VR</h2>
-        <button onClick={() => socket.emit("command", JSON.stringify({ 'vl': 0, 'vr': 5, 'hl': 0, 'hr': 0 }))} style={{height: "3rem", width: "6rem"}}>+</button>
-        <button onClick={() => socket.emit("command", JSON.stringify({ 'vl': 0, 'vr': -5, 'hl': 0, 'hr': 0 }))} style={{height: "3rem", width: "6rem"}}>-</button>
+        <button onClick={() => {axios.get(`${URL}/command/vr/5`)}} style={{height: "3rem", width: "6rem"}}>+</button>
+        <button onClick={() => {axios.get(`${URL}/command/vr/-5`)}} style={{height: "3rem", width: "6rem"}}>-</button>
       </div>
       <br/>
       <div style={{backgroundColor: "yellow", borderRadius: "6px", padding: "1rem"}} className="hr">
         <h2>HR</h2>
-        <button onClick={() => socket.emit("command", JSON.stringify({ 'vl': 0, 'vr': 0, 'hl': 0, 'hr': 5 }))} style={{height: "3rem", width: "6rem"}}>+</button>
-        <button onClick={() => socket.emit("command", JSON.stringify({ 'vl': 0, 'vr': 0, 'hl': 0, 'hr': -5 }))} style={{height: "3rem", width: "6rem"}}>-</button>
+        <button onClick={() => {axios.get(`${URL}/command/hr/5`)}} style={{height: "3rem", width: "6rem"}}>+</button>
+        <button onClick={() => {axios.get(`${URL}/command/hr/-5`)}} style={{height: "3rem", width: "6rem"}}>-</button>
       </div>
 
     </div>
