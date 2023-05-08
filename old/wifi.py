fa5c89da4c8db1e09caaf6e8141f93c4e477db11
Paul Finkbeiner <paul_finkbeiner@arburg.com>
2023-05-08 10:14:02 +0200

    initial commit with simple gRPC example

--- old/wifi.py
+++
@@ -1,6 +0,0 @@
-# Dependencies : sudo apt install python3-dev python3-pip && sudo pip3 install wireless netifaces psutil
-# Installation: sudo apt update && sudo apt --yes --force-yes install dnsmasq hostapd python3-dev python3-pip && sudo pip3 install pyaccesspoint
-
-from PyAccessPoint import pyaccesspoint
-
-access_point = pyaccesspoint.AccessPoint()
---
+++ python/script.py
@@ -0,0 +1,20 @@
+from __future__ import print_function
+
+import logging
+
+import grpc
+import hello_pb2
+import hello_pb2_grpc
+
+
+def run():
+    print("Will try to greet world ...")
+    with grpc.insecure_channel('localhost:50051') as channel:
+        stub = hello_pb2_grpc.GreeterStub(channel)
+        response = stub.SayHello(hello_pb2.HelloRequest(name='you'))
+    print("Greeter client received: " + response.message)
+
+
+if __name__ == '__main__':
+    logging.basicConfig()
+    run()
