8f8eaf76abb5e9838a6d1d13add78f0624d498c0
pafinkbeiner <paul-d-f@web.de>
2023-05-14 14:46:37 +0200

    sc

--- prod/pwm/script.py
+++ prod/pwm/script.py
@@ -21,9 +21,9 @@ GPIO.setup(ledpin,GPIO.OUT)
 pi_pwm = GPIO.PWM(ledpin,1000)		    #create PWM instance with frequency
 pi_pwm.start(0)				            #start PWM of required Duty Cycle 
 pi_pwm.ChangeDutyCycle(50)
-pi_pwm.ChangeDutyCycle(100)
-while True:
-    time.sleep(1)
+# pi_pwm.ChangeDutyCycle(100)
+# while True:
+#     time.sleep(1)
 
 
 # for speed in range(10, 100, 5):
@@ -31,15 +31,15 @@ while True:
 #     pi_pwm.ChangeDutyCycle(speed)
 #     time.sleep(3)
 
-# for speed in range(900, MAX_PWM_SPEED, 100):
-#     for duty in range(10, 100, 10):
-#         print("Speed: "+ str(speed) + " duty: "+ str(duty))
-#         pi_pwm.stop()
-#         pi_pwm.ChangeFrequency(speed)
-#         time.sleep(0.3)
-#         print(speed)
-#         pi_pwm.start(duty)
-#         time.sleep(1)
+for speed in range(900, MAX_PWM_SPEED, 100):
+    for duty in range(10, 100, 10):
+        print("Speed: "+ str(speed) + " duty: "+ str(duty))
+        pi_pwm.stop()
+        pi_pwm.ChangeFrequency(speed)
+        time.sleep(0.3)
+        print(speed)
+        pi_pwm.start(duty)
+        time.sleep(1)
 
 # set pwm to 1600
 # pi_pwm.ChangeDutyCycle(1600)
