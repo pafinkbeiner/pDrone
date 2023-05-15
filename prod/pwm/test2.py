67979a125d0f08b788ec1bd99749963eafe2504a
pafinkbeiner <paul-d-f@web.de>
2023-05-15 18:51:33 +0200

    sc

--- prod/pwm/test2.py
+++ prod/pwm/test2.py
@@ -1,7 +1,7 @@
 import time   
 import pigpio 
 
-ESC=19
+ESC=18
 
 pi = pigpio.pi()
 pi.set_servo_pulsewidth(ESC, 0) 
@@ -10,6 +10,20 @@ max_value = 2000 #change this if your ESC's max value is different or leave it b
 min_value = 700  #change this if your ESC's min value is different or leave it be
 
 print("CALIBRATE")
+print("Setting motors to zero!")
+pi.set_servo_pulsewidth(ESC, 0)
+print("Disconnect the battery (7sec)")
+time.sleep(3)
+pi.set_servo_pulsewidth(ESC, max_value)
+print("Connect the battery NOW.. (7sec)")
+time.sleep(3)
+pi.set_servo_pulsewidth(ESC, min_value)
+print("Wait for 15 seconds")
+time.sleep(5)
+pi.set_servo_pulsewidth(ESC, 0)
+time.sleep(1)
+print("Arming ESC. Setting motor values to min value")
+pi.set_servo_pulsewidth(ESC, min_value)
 
 
 print("ARM")
