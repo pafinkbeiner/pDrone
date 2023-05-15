ae51e14b914af71e320a21953ede40d2e6a75e37
pafinkbeiner <paul-d-f@web.de>
2023-05-15 18:49:35 +0200

    sc

--- prod/pwm/test2.py
+++ prod/pwm/test2.py
@@ -1,7 +1,7 @@
 import time   
 import pigpio 
 
-ESC=12 
+ESC=18
 
 pi = pigpio.pi()
 pi.set_servo_pulsewidth(ESC, 0) 
@@ -9,6 +9,10 @@ pi.set_servo_pulsewidth(ESC, 0)
 max_value = 2000 #change this if your ESC's max value is different or leave it be
 min_value = 700  #change this if your ESC's min value is different or leave it be
 
+print("CALIBRATE")
+
+
+print("ARM")
 pi.set_servo_pulsewidth(ESC, 0)
 time.sleep(1)
 pi.set_servo_pulsewidth(ESC, max_value)
