42aecdd61073922d06498f3797a1f97e0877999a
pafinkbeiner <paul-d-f@web.de>
2023-05-15 18:31:00 +0200

    sc

--- prod/pwm/script.py
+++ prod/pwm/script.py
@@ -18,10 +18,11 @@ GPIO.setup(ledpin,GPIO.OUT)
 
 
 # # start pwm
-pi_pwm = GPIO.PWM(ledpin, 1500)
+pi_pwm = GPIO.PWM(ledpin, 16000)
 pi_pwm.start(0)				      
 
 for speed in range(1, 100, 10):
+    print(speed)
     pi_pwm.ChangeDutyCycle(speed)
     time.sleep(3)
         
