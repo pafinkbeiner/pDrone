6c8907f991a77ed6f31373ce9a077518fabac2aa
pafinkbeiner <paul-d-f@web.de>
2023-05-15 18:46:53 +0200

    sc

---
+++ prod/pwm/test2.py
@@ -0,0 +1,26 @@
+import time   
+import pigpio 
+
+ESC=12 
+
+pi = pigpio.pi()
+pi.set_servo_pulsewidth(ESC, 0) 
+
+max_value = 2000 #change this if your ESC's max value is different or leave it be
+min_value = 700  #change this if your ESC's min value is different or leave it be
+
+pi.set_servo_pulsewidth(ESC, 0)
+time.sleep(1)
+pi.set_servo_pulsewidth(ESC, max_value)
+time.sleep(1)
+pi.set_servo_pulsewidth(ESC, min_value)
+time.sleep(1)
+
+for speed in range(min_value, max_value, 100):
+    print(speed)
+    pi.set_servo_pulsewidth(ESC, speed)
+    time.sleep(1)
+
+
+pi.set_servo_pulsewidth(ESC, 0)
+pi.stop()
