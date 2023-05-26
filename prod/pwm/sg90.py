34d274d50960f62fcfbc4fe46e7720027ef09a0c
pafinkbeiner <paul-d-f@web.de>
2023-05-26 17:19:45 +0200

    sc

--- prod/pwm/sg90.py
+++ prod/pwm/sg90.py
@@ -9,20 +9,18 @@ GPIO.setup(12, GPIO.OUT)
 pi_pwm = GPIO.PWM(12, 50)
 pi_pwm.start(0)				      
 
-pi_pwm.ChangeDutyCycle(50) 
-time.sleep(3)
 
-# for duty in range(0,101,1):
-#     pi_pwm.ChangeDutyCycle(duty) 
-#     time.sleep(0.01)
+for duty in range(0,101,1):
+    pi_pwm.ChangeDutyCycle(duty) 
+    time.sleep(0.01)
 
-# time.sleep(0.5)
+time.sleep(0.5)
 
-# for duty in range(100,-1,-1):
-#     pi_pwm.ChangeDutyCycle(duty)
-#     time.sleep(0.01)
+for duty in range(100,-1,-1):
+    pi_pwm.ChangeDutyCycle(duty)
+    time.sleep(0.01)
 
-# time.sleep(0.5)
+time.sleep(0.5)
 
 pi_pwm.stop()
 GPIO.cleanup()
