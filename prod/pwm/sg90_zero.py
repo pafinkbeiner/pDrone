4d2ac667601c7fd4e932d505f96982c3525e85dc
pafinkbeiner <paul-d-f@web.de>
2023-05-26 17:33:10 +0200

    sc

--- prod/pwm/sg90_zero.py
+++ prod/pwm/sg90_zero.py
@@ -1,11 +1,11 @@
 from gpiozero import Servo
 from time import sleep
-
 from gpiozero.pins.pigpio import PiGPIOFactory
+
 
 factory = PiGPIOFactory()
 
-servo = Servo(12, min_pulse_width=0.5/1000, max_pulse_width=2.5/1000, pin_factory=factory)
+servo = Servo(18, min_pulse_width=0.5/1000, max_pulse_width=2.5/1000, pin_factory=factory)
 
 print("Start in the middle")
 servo.mid()
